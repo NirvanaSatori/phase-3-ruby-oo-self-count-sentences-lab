333ed0187ae7f5e2c257fa1dceadf9f586eaea37
MORRIS <morris24kariuki@gmail.com>
2023-02-11 19:06:39 +0300

    assignment cnmpleted

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with? (".")
   end
 
   def question?
-
+    self.end_with? ("?")
   end
 
   def exclamation?
-
+    self.end_with? ("!")
   end
 
   def count_sentences
-
+    self.split(/[.?!]+/).count
   end
 end
+
+puts "hello there./".sentence?
+puts "hello there.!?".question?
+puts "hello there?".exclamation?
+puts "Hey you! Yes you.Are you listening?".count_sentences
